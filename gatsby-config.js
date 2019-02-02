e121fb1d3651dda959e883a2c810d583611ffd63
ra9 <cnah27@gmail.com>
2019-02-02 16:03:58 +0000

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  siteMetadata: {
+    title: 'CodeWithCarlos',
+    author: 'ra9',
+    authorLink: 'https://github.com/ra9',
+    disqus: 'gatsby-starter-blog'// put your disqus ID here
+  },
+  plugins: [
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages'
+      }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/examples`,
+        name: 'examples'
+      }
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          'gatsby-remark-prismjs',
+          'gatsby-remark-copy-linked-files'
+        ]
+      }
+    },
+    'gatsby-plugin-offline',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-sass',
+      options: {
+        includePaths: [`${__dirname}/node_modules`, `${__dirname}/src/`],
+        precision: 8
+      }
+    }
+  ]
+}
